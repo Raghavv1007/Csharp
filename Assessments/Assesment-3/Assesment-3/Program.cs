cd1deddcbbd005f5fb751ddc58186008b828f9a5
Raghav Garg <raghavg@infinite.com>
2024-07-03 12:52:06 +0530

    Assesment-3CC3

---
+++ Assessments/Assesment-3/Assesment-3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assesment_3
+{
+    class Cricket
+    {
+        public List<int> scores = new List<int>();
+
+        public void Pointscalculation(int no_of_matches)
+        {
+            Console.WriteLine($"Enter scores for {no_of_matches} matches:");
+            for (int i = 0; i < no_of_matches; i++)
+            {
+                Console.Write($"Match {i + 1} score: ");
+                int score = Convert.ToInt32(Console.ReadLine());
+                scores.Add(score);
+            }
+
+            int total_score = scores.Sum();
+            double average_score = (double)total_score / no_of_matches;
+            Console.WriteLine($"\nTotal Sum of Scores: {total_score}");
+            Console.WriteLine($"Average Score: {average_score:F2}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Cricket ipl = new Cricket();
+            int no_of_matches;
+
+            Console.Write("Enter number of matches: ");
+            no_of_matches = Convert.ToInt32(Console.ReadLine());
+
+            ipl.Pointscalculation(no_of_matches);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assessments/Assesment-3/Assesment-3/Program2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assesment_3
+{
+    using System;
+
+    class Box
+    {
+        public int Length { get; set; }
+        public int Breadth { get; set; }
+
+        public Box(int length, int breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        public static Box AddBox(Box box1, Box box2)
+        {
+            Box result = new Box(box1.Length + box2.Length, box1.Breadth + box2.Breadth);
+            return result;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Length: {Length}, Breadth: {Breadth}");
+        }
+    }
+
+    class Test
+    {
+        static void Main()
+        {
+            Box box1 = new Box(7, 12);
+            Box box2 = new Box(8, 4);
+
+            Console.WriteLine("Box 1 details:");
+            box1.Display();
+
+            Console.WriteLine("Box 2 details:");
+            box2.Display();
+
+            Box box3 = Box.AddBox(box1, box2);
+
+            
+            Console.WriteLine("Box 3 details (Add Box 1 and Box 2):");
+            box3.Display();
+            Console.ReadLine();
+        }
+    }
+    
+
+    
+}

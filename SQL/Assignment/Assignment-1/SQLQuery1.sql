e8a422744a9e858fbbdc8a58f845f1e579ea2823
Raghav Garg <raghavg@infinite.com>
2024-07-11 18:13:50 +0530

    SQL

--- SQL/Assignment/Assignment-1/SQLQuery1.sql
+++ SQL/Assignment/Assignment-1/SQLQuery1.sql
@@ -57,6 +57,7 @@ VALUES
   (1002, 'Trackon Consultants', 'Mumbai', 'consult@trackon.com', '8734210090', 'Consultant'),
   (1003, 'MoneySaver Distributors', 'Kolkata', 'save@moneysaver.com', '7799886655', 'Reseller'),
   (1004, 'Lawful Corp', 'Chennai', 'justice@lawful.com', '9210342219', 'Professional');
+  select * from Clients
 
 INSERT INTO Departments (Deptno, Dname, Loc)
 VALUES
@@ -64,6 +65,7 @@ VALUES
   (20, 'Development', 'Pune'),
   (30, 'Testing', 'Mumbai'),
   (40, 'Document', 'Mumbai');
+  select * from Departments
 
 INSERT INTO Employeees (Empno, Ename, Job, Salary, Deptno)
 VALUES
@@ -78,6 +80,7 @@ VALUES
   (7009, 'Nutan', 'Developer', 45000.00, 20),
   (7010, 'Smita', 'Analyst', 20000.00, 10),
   (7011, 'Anand', 'Project Mgr', 65000.00, 10);
+  select * from Employeees
 
 INSERT INTO Projectss (Project_ID, Descr, Start_Date, Planned_End_Date, Actual_End_date, Budget, Client_ID)
 VALUES
@@ -85,8 +88,23 @@ VALUES
   (402, 'Accounting', '2011-08-01', '2012-01-01', NULL, 500000.00, 1002),
   (403, 'Payroll', '2011-10-01', '2011-12-31', NULL, 75000.00, 1003),
   (404, 'Contact Mgmt', '2011-11-01', '2011-12-31', NULL, 50000.00, 1004);
+  select * from Projectss
 
 INSERT INTO EmpProjectTasks (Project_ID, Empno, Start_Date, End_Date, Task, Status)
 VALUES
-  (401, 7001, '2011-04-01', '2011-04-20', 'Task1', 'In Progress');
+ (401, 7001, '2011-04-01', '2011-04-20', 'System Analysis', 'Completed'),
+ (401, 7002, '2011-04-21', '2011-05-30', 'System Design', 'Completed'),
+ (401, 7003, '2011-06-01', '2011-07-15', 'Coding', 'Completed'),
+ (401, 7004, '2011-07-18', '2011-09-01', 'Coding', 'Completed'),
+ (401, 7006, '2011-09-03', '2011-09-15', 'Testing', 'Completed'),
+ (401, 7009, '2011-09-18', '2011-10-05', 'Code Change', 'Completed'),
+ (401, 7008, '2011-10-06', '2011-10-16', 'Testing', 'Completed'),
+ (401, 7007, '2011-10-06', '2011-10-22', 'Documentation', 'Completed'),
+ (401, 7011, '2011-10-22', '2011-10-31', 'Sign off', 'Completed'),
+ (402, 7010, '2011-08-01', '2011-08-20', 'System Analysis', 'Completed'),
+ (402, 7002, '2011-08-22', '2011-09-30', 'System Design', 'Completed'),
+ (402, 7004, '2011-10-01', NULL, 'Coding', 'In Progress');
+ 
+
+select * from EmpProjectTasks
 
